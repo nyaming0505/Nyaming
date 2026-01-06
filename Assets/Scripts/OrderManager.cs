9fff94a722e190b65d27f493f2c9add42e369ed7
aaws11373ada@gmail.com <aaws11373ada@gmail.com>
2026-01-06 19:15:15 +0900

    [2026-01-06] 루트 -OrderManager.cs 하트 연결

--- Assets/Scripts/OrderManager.cs
+++ Assets/Scripts/OrderManager.cs
@@ -89,6 +89,11 @@ public class OrderManager : MonoBehaviour
     {
         Debug.Log("[ORDER] 시간 초과");
 
+        if (HeartManager.instance != null)
+        {
+            HeartManager.instance.OnOrderFailed();
+        }
+
         order.customer.OnTimeOver();
 
         if (order.uiItem != null)
@@ -187,6 +192,7 @@ public class OrderManager : MonoBehaviour
         else
         {
             Debug.Log("[ORDER] 실패");
+            HeartManager.instance.OnOrderFailed();
             targetCustomer.OnTimeOver();
         }
 
