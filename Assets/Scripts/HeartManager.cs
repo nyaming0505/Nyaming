d14eabdda2abd09451377767ee795331beff5c49
aaws11373ada@gmail.com <aaws11373ada@gmail.com>
2026-01-07 20:17:27 +0900

    [2026-01-07] 루트 - 더미코드 제거 및 5초안에 10초 클릭 완료

--- Assets/Scripts/HeartManager.cs
+++ Assets/Scripts/HeartManager.cs
@@ -26,18 +26,6 @@ public class HeartManager : MonoBehaviour
         ResetHearts();
     }
 
-    void Update()
-    {
-        // 🧪 [테스트용] 스페이스바를 누르면 손님 주문 실패 상황을 가정함
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("테스트: 주문 실패! 하트 감소!");
-            OnOrderFailed();
-        }
-    }
-
-
-    // 손님 주문 실패 시 호출될 함수
     public void OnOrderFailed()
     {
         if (currentHearts > 0)
@@ -52,29 +40,23 @@ public class HeartManager : MonoBehaviour
         }
     }
 
-    // UI 이미지를 현재 하트 개수에 맞춰 바꿔주는 함수
     void UpdateHeartUI()
     {
         for (int i = 0; i < heartImages.Length; i++)
         {
             if (i < currentHearts)
             {
-                // 현재 하트 개수보다 인덱스가 작으면 꽉 찬 하트
                 heartImages[i].sprite = fullHeartSprite;
             }
             else
             {
-                // 그 외에는 빈 하트
                 heartImages[i].sprite = emptyHeartSprite;
             }
         }
     }
 
-    // 게임오버 처리
     void GameOver()
     {
-        Debug.Log("💀 하트 0개! 게임 오버!");
-
         if (GameOverManager.Instance != null)
         {
             GameOverManager.Instance.TriggerGameOver();
